269a98119f448b3457b11b1378266e9dc11a5506
Martin Disch <martindisch@gmail.com>
2018-04-29 17:56:58 +0200

    Implement basic WAV recording

--- app/src/main/java/com/martindisch/announcer/MainActivity.java
+++ app/src/main/java/com/martindisch/announcer/MainActivity.java
@@ -1,13 +1,119 @@
 package com.martindisch.announcer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private boolean permissionToRecordAccepted = false;
+    private String[] permissions = {Manifest.permission.RECORD_AUDIO};
+
+    private Button mRecordButton, mPlayButton;
+    private boolean mRecording = false;
+    private RecordWaveTask recordTask = null;
+    private String mFileName = null;
+    private MediaPlayer mPlayer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecordButton = findViewById(R.id.bRecord);
+        mRecordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!mRecording) {
+                    mRecordButton.setText(R.string.record_stop);
+                    mPlayButton.setEnabled(false);
+                    launchTask();
+                } else {
+                    recordTask.cancel(false);
+                    mRecordButton.setText(R.string.record_start);
+                    mPlayButton.setEnabled(true);
+                }
+                mRecording = !mRecording;
+            }
+        });
+        mPlayButton = findViewById(R.id.bPlay);
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPlaying();
+            }
+        });
+
+        // Prepare the record task
+        recordTask = new RecordWaveTask();
+
+        // Record to the external cache directory for visibility
+        mFileName = getExternalCacheDir().getAbsolutePath();
+        mFileName += "/message.wav";
+
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
     }
+
+    private void startPlaying() {
+        mRecordButton.setEnabled(false);
+        mPlayer = new MediaPlayer();
+        try {
+            mPlayer.setDataSource(mFileName);
+            mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mRecordButton.setEnabled(true);
+                }
+            });
+            mPlayer.prepare();
+            mPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void launchTask() {
+        switch (recordTask.getStatus()) {
+            case FINISHED:
+                recordTask = new RecordWaveTask();
+                break;
+            case PENDING:
+                if (recordTask.isCancelled()) {
+                    recordTask = new RecordWaveTask();
+                }
+        }
+        File wavFile = new File(mFileName);
+        recordTask.execute(wavFile);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted) finish();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
 }
