9991da9fc86a842696f2378cec0f5cd5d8ca240d
Martin Disch <martindisch@gmail.com>
2018-05-05 15:47:11 +0200

    Implement sending and playing of message over SSH

--- app/src/main/java/com/martindisch/announcer/MainActivity.java
+++ app/src/main/java/com/martindisch/announcer/MainActivity.java
@@ -1,15 +1,25 @@
 package com.martindisch.announcer;
 
 import android.Manifest;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpException;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,10 +27,12 @@ import java.io.IOException;
 public class MainActivity extends AppCompatActivity {
 
     private Button mRecordButton, mPlayButton, mUploadButton;
+    private EditText mHost, mPort;
     private boolean mRecording = false;
     private RecordWaveTask recordTask = null;
     private String mFileName = null;
     private MediaPlayer mPlayer = null;
+    private Session mSession = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,9 +66,50 @@ public class MainActivity extends AppCompatActivity {
         mUploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Save entered connection details to preferences
+                SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+                SharedPreferences.Editor editor = prefs.edit();
+                final String host = mHost.getText().toString();
+                final int port = Integer.parseInt(mPort.getText().toString());
+                editor.putString("host", host);
+                editor.putInt("port", port);
+                editor.apply();
 
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            // Set up JSch
+                            JSch.setConfig("StrictHostKeyChecking", "no");
+                            JSch jsch = new JSch();
+                            File privateKey = new File(Environment.getExternalStorageDirectory(), "Announcer/id_rsa");
+                            jsch.addIdentity(privateKey.getAbsolutePath());
+                            mSession = jsch.getSession("root", host, port);
+                            mSession.connect();
+
+                            // Start SFTP channel to upload the message
+                            ChannelSftp channelsftp = (ChannelSftp) mSession.openChannel("sftp");
+                            channelsftp.connect();
+                            channelsftp.put(getExternalCacheDir().getAbsolutePath() + "/message.wav", "message.wav");
+                            channelsftp.disconnect();
+
+                            // Start SSH channel to play the message
+                            ChannelExec channelssh = (ChannelExec) mSession.openChannel("exec");
+                            channelssh.setCommand("aplay message.wav");
+                            channelssh.connect();
+                            channelssh.disconnect();
+                        } catch (JSchException | SftpException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
             }
         });
+        mHost = findViewById(R.id.etHost);
+        mPort = findViewById(R.id.etPort);
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        mHost.setText(prefs.getString("host", ""));
+        mPort.setText(String.format("%d", prefs.getInt("port", 22)));
 
         // Prepare the record task
         recordTask = new RecordWaveTask();
@@ -117,6 +170,9 @@ public class MainActivity extends AppCompatActivity {
         if (mPlayer != null) {
             mPlayer.release();
             mPlayer = null;
+        }
+        if (mSession != null) {
+            mSession.disconnect();
         }
     }
 
